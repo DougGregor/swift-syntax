11bbad12723f85d2235926399f06f77c0d9064ab
Doug Gregor <dgregor@apple.com>
2022-09-15 14:21:29 -0700

    Add nodes to describe general macro expansions

--- Sources/generate-swift-syntax-builder/gyb_syntax_support/NodeSerializationCodes.py
+++ Sources/generate-swift-syntax-builder/gyb_syntax_support/NodeSerializationCodes.py
@@ -281,6 +281,8 @@ SYNTAX_NODE_SERIALIZATION_CODES = {
     'ConventionWitnessMethodAttributeArguments': 277,
     'DesignatedTypeElement': 278,
     'NamedOpaqueReturnType': 279,
+    'MacroExpansionExpr': 280,
+    'MacroExpansionDecl': 281,
 }
 
 
